e926040e972c26ce47d5f13971b42be8ac21adb6
beovan <truongvangiap04@gmail.com>
2024-05-05 09:00:11 +0700

    feat: add more css ,js and picture

--- pythonweb/settings.py
+++ pythonweb/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        'app',
+
 ]
 
 MIDDLEWARE = [
@@ -55,13 +57,14 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True,
+       'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
