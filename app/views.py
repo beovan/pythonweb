3695447a96a68c555ff82bf6dfbd84db4113affa
beovan <truongvangiap04@gmail.com>
2024-05-10 11:39:46 +0700

    Refactor: add lazy loading for images and update cart.html template
    - Add 'image' field to the Product model in models.py
    - Implement lazy loading for images in cart.html template

--- app/views.py
+++ app/views.py
@@ -6,8 +6,14 @@ def home(request):
     products = Product.objects.all()
     context = {'products': products} 
     return render(request, 'home.html',context)
-def cart(request): 
-    context={}
+def cart(request):
+    if request.user.is_authenticated:
+        custormer = request.user.custormer
+        order, created = Order.objects.get_or_create(custormer = custormer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items, 'order': order}
     return render(request, 'cart.html',context)
 def checkout(request): 
     context={}
