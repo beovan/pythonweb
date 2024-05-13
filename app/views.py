c81f7fd2426a71f393b926eee9c381f6c63a151f
beovan <truongvangiap04@gmail.com>
2024-05-13 21:34:22 +0700

    chore: Register Customer model in admin.py, add register and login paths, and update templates

--- app/views.py
+++ app/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse ,JsonResponse
 from.models import *
 import json
+from django.contrib.auth.forms import UserCreationForm
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'register.html', context)
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
 def home(request):
     products = Product.objects.all()
     context = {'products': products} 
