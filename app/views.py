5f7322d2e13dc73ddf2caa87ceb9b6a258e2ac5b
beovan <truongvangiap04@gmail.com>
2024-05-15 22:59:47 +0700

    Refactor: add lazy loading for images and update cart.html template

--- app/views.py
+++ app/views.py
@@ -1,32 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse ,JsonResponse
 from.models import *
 import json
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 def register(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
+    
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
     context = {'form': form}
     return render(request, 'register.html', context)
-def login(request):
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else: messages.info(request, 'Username or Password is incorrect')
+        
+        
     context = {}
     return render(request, 'login.html', context)
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
 def home(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total': 0}
+        cartItems = order['get_cart_items']
     products = Product.objects.all()
-    context = {'products': products} 
+    context = {'products': products, 'cartItems': cartItems} 
+    
     return render(request, 'home.html',context)
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_items': 0, 'get_cart_total': 0}
-    context = {'items': items, 'order': order}
+        cartItems = order['get_cart_items']
+
+    context = {'items': items, 'order': order,'cartItems': cartItems}
     return render(request, 'cart.html', context)
 
 def checkout(request):
